cf0df427251cf73e97a49f0d05e5979a90647750
lucamartinsb <lucabindermartins@gmail.com>
2025-11-10 20:00:16 -0300

    chore: instalação e configuração do módulo CORS

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 const express = require('express'); //importa o módulo express
-const app = express(); //cria uma aplicação express
+const cors = require('cors'); //importa o módulo cors para permitir requisições de diferentes origens
 const port = process.env.PORT || 3000; //define a porta onde o servidor irá rodar
 require('dotenv').config(); //carrega variáveis de ambiente do arquivo .env
 const mongoose = require('mongoose'); //importa o mongoose para conectar ao MongoDB
@@ -7,19 +7,21 @@ const mongo_uri = process.env.MONGO_URI //define a URI do MongoDB
 const Product = require('./models/Product'); //importa o modelo Product
 
 mongoose.connect(mongo_uri)
-    .then(() => {
-        console.log('Conectado ao MongoDB');
-        // Inicia o servidor na porta definida
-        app.listen(port, () => {
-            console.log(`Servidor rodando em http://localhost:${port}`); //mensagem no console quando o servidor iniciar
+.then(() => {
+    console.log('Conectado ao MongoDB');
+    // Inicia o servidor na porta definida
+    app.listen(port, () => {
+        console.log(`Servidor rodando em http://localhost:${port}`); //mensagem no console quando o servidor iniciar
     });
 }).catch((error) => { console.error('Erro ao conectar ao MongoDB:', error) }); //conecta ao MongoDB
 
-// Middleware para analisar o corpo das requisições como JSON
-app.use(express.json());
+const app = express(); //cria uma aplicação express
 
-// Middleware para analisar o corpo das requisições como URL-encoded
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json()); // Middleware para o Express entender os dados (JSON) que vêm do frontend
+
+app.use(cors()); // É um "selo de permissão" que permite que o seu site (em uma porta, ex: 5173) converse com o seu servidor (em outra porta, ex: 3000). Sem isso, o navegador bloqueia tudo
+
+app.use(express.urlencoded({ extended: true })); // Middleware para analisar o corpo das requisições como URL-encoded
 
 // Rota GET para a url raiz do servidor
 app.get('/', (req, res) => {
